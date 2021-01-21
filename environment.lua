72bd76d510002dbe802a705bfb021b552d908a68
Robert Kalescky <rkalescky@smu.edu>
2021-01-21 08:21:47 -0600

    Use Docker Hub for container.

--- environment.lua
+++ environment.lua
@@ -1,16 +1,6 @@
 require("lfs")
 always_load("singularity")
 
-local sif_hash      = 'f4b57eb40aaecb8f75c6d972ea382bbd9a884ad6cbecee22f2adba8f9555c70a'
-local sif_directory = os.getenv("WORK") .. '/singularity_containers/'
-local sif_file      = sif_directory .. 'itom_web_scraping_sha256.' .. sif_hash .. '.sif'
-
-if not lfs.attributes(sif_file) then
-  lfs.mkdir(sif_directory)
-  local sif_download = 'singularity pull ' .. sif_file .. ' library://rkalescky/default/itom_web_scraping:sha256.' .. sif_hash
-  capture(sif_download)
-end
-
 function use_xvfb()
   local value=os.getenv("USE_XVFB")
   if (value == nil or value == '' or value == '1' or value:lower() == 'true') then
@@ -21,17 +11,14 @@ function use_xvfb()
 end
 
 function build_command(app)
+  local container     = 'docker://smuresearch/scraping_tools:latest '
   local cmd_beginning = 'singularity exec --writable-tmpfs -B /scratch,/work,/run/user '
-  local cmd_ending    = sif_file .. use_xvfb()
-  local sh_ending     = ' "$@"'
-  local csh_ending    = ' $*'
-  local sh_cmd        = cmd_beginning .. cmd_ending .. app .. sh_ending
-  local csh_cmd       = cmd_beginning .. cmd_ending .. app .. csh_ending
+  local sh_cmd        = cmd_beginning .. container .. use_xvfb() .. app .. ' "$@"'
+  local csh_cmd       = cmd_beginning .. container .. use_xvfb() .. app .. ' $*'
   set_shell_function(app , sh_cmd, csh_cmd)
 end
 
-setenv("TMPDIR", "/dev/shm")
-prepend_path("PATH", lfs.currentdir())
+setenv('TMPDIR', '/dev/shm')
 
 build_command('python3')
 build_command('ipython3')
